ca4aa23e6c8ebce818344adfdf2827aa91d8542a
novianaesbl <127191762+novianaesbl@users.noreply.github.com>
2023-03-26 12:45:42 +0700

    deploy

---
+++ application/views/v_landingpage.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>landing page spp</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: Bootslander
+  * Updated: Mar 10 2023 with Bootstrap v5.2.3
+  * Template URL: https://bootstrapmade.com/bootslander-free-bootstrap-landing-page-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body style = "font-family:georgia,garamond,serif !important;">
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+        <div class="container d-flex align-items-center justify-content-between">
+
+            <div class="logo">
+                <h1><a href="login"><span>Selamat Datang</span></a></h1>
+                <!-- Uncomment below if you prefer to use an image logo -->
+                <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+            </div>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                    <li><a class="nav-link scrollto" href="#about">About</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero">
+
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-lg-7 pt-5 pt-lg-0 order-2 order-lg-1 d-flex align-items-center">
+                    <div data-aos="zoom-out">
+                        <h1>Ini adalah aplikasi pembayaran <span>Spp</span></h1>
+
+                        <div class="text-center text-lg-start">
+                            <a href="login" class="btn-get-started scrollto">Login sekarang!</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 order-1 order-lg-2 Logo SMKIP-png" data-aos="zoom-out" data-aos-delay="300">
+                    <img src="assets/img/Logo SMKIP.png" class="img-fluid animated" alt="">
+                </div>
+            </div>
+        </div>
+
+        <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+            viewBox="0 24 150 28 " preserveAspectRatio="none">
+            <defs>
+                <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
+            </defs>
+            <g class="wave1">
+                <use xlink:href="#wave-path" x="50" y="3" fill="rgba(255,255,255, .1)">
+            </g>
+            <g class="wave2">
+                <use xlink:href="#wave-path" x="50" y="0" fill="rgba(255,255,255, .2)">
+            </g>
+            <g class="wave3">
+                <use xlink:href="#wave-path" x="50" y="9" fill="#fff">
+            </g>
+        </svg>
+
+    </section><!-- End Hero -->
+
+    <main id="main">
+
+        <!-- ======= About Section ======= -->
+        <section id="about" class="about">
+            <div class="container-fluid">
+
+                <div class="row">
+
+
+                </div>
+
+                <div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5"
+                    data-aos="fade-left">
+                    <h3>Tentang Aplikasi</h3>
+                    <p>Aplikasi pembayaran SPP merupakan suatu sistem yang dapat digunakan untuk
+                        melakukan transaksi pembayaran SPP (sumbangan pembinaan pendidikan) agar tidak mengeluarkan
+                        banyak begitu kertas dan agar memudahkan pembayarn spp, dan menjadi lebih simple tidak
+                        ribet, jia ingin mencari data spp.</p>
+                </div>
+
+            </div>
+            </div>
+
+            </div>
+        </section><!-- End About Section -->
+
+
+        <!-- ======= Details Section ======= -->
+        <section id="details" class="details">
+            <div class="container">
+
+                <div class="row content">
+                    <div class="col-md-4" data-aos="fade-right">
+                        <img src="assets/img/download.jpg" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-md-8 pt-4" data-aos="fade-up">
+                        <h3></h3>Aplikasi spp menyediakan beberapa fitur
+                        <p class="fst-italic">
+                            Pertama fitur login dimiliki oleh :
+                        </p>
+                        <ul>
+                            <li><i class="bi bi-check"></i> Admin
+                            </li>
+                            <li><i class="bi bi-check"></i> Petugas
+                            </li>
+                            <li><i class="bi bi-check"></i> Siswa</li>
+                            <p>
+                                Login ini dibuat untuk menjaga keamanan data siswa petugas dan admin dan tidak sembarang
+                                org yg bisa membukanya.
+                            </p>
+                    </div>
+                </div>
+            </div>
+            </div>
+
+            <div class="row content">
+                <div class="col-md-4" data-aos="fade-right">
+                    <img src="assets/img/download(1).jpg" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-8 pt-5" data-aos="fade-up">
+                    <h3>Kelebihan Menggunakan Aplikasi Pembayaran SPP</h3>
+                    <p>Fitur dapat disesuaikan dengan keperluan sekolah. Aplikasi ringan, langsung terintegrasi
+                        dengan sistem keuangan sekolah dalam bentuk software add-on. Memudahkan pengelolaan keuangan
+                        sekolah menjadi lebih komprehensif. Laporan terdata secara real-time.</p>
+                </div>
+            </div>
+
+            <div class="row content">
+                <div class="col-md-4 order-1 order-md-2" data-aos="fade-left">
+                </div>
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="footer-info">
+                            <h3>ib:</h3>
+                            <p class="pb-3"><em>sebaik baiknya berharap, jangan berharap :D
+                                    <div class="social-links mt-3">
+                                        <a href="https://twitter.com/?lang=id" class="twitter"><i
+                                                class="bx bxl-twitter"></i></a>
+                                        <a href="https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwiontzvsdP9AhU8h0sFHbeIA-sYABAAGgJzZg&ohost=www.google.com&cid=CAASJeRoLzkcu-kHoJbilsmm3ZkGP7d8PbHWhD1GP8AWuY5KcVsvRBY&sig=AOD64_30a9Qc7LtIXjfqWBklhJu2P2I7uQ&q&adurl&ved=2ahUKEwjiudXvsdP9AhWZ4TgGHTUyAVYQ0Qx6BAgKEAE"
+                                            class="facebook"><i class="bx bxl-facebook"></i></a>
+                                        <a href="https://www.instagram.com/?hl=id" class="instagram"><i
+                                                class="bx bxl-instagram"></i></a>
+                                        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                                        <a href="https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwjDsIu5stP9AhVVDisKHSYqAj4YABAAGgJzZg&ohost=www.google.com&cid=CAASJeRo0ASVQB7GmsMloWyBvLN6ktfx560Qb1pddkP_usnXUUiSjsE&sig=AOD64_3ZwYbbb98WTJM4gVtrlnqt37rDsg&q&adurl&ved=2ahUKEwiu-YS5stP9AhW81jgGHeezAqgQ0Qx6BAgLEAE"
+                                            class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                                    </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Riri</span></strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bootslander-free-bootstrap-landing-page-template/ -->
+                Designed by <a href="https://bootstrapmade.com/">Rianti Eka Saputri.</a>
+            </div>
+        </div>
+    </footer><!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+    <div id="preloader"></div>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
